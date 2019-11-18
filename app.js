ad8ce1d49b3223eab76f443eef4be3cf19374280
Arbias <52889109+k-arbias@users.noreply.github.com>
2019-11-18 19:55:02 +0100

    Finished the project

--- app.js
+++ app.js
@@ -9,8 +9,19 @@ function getJokes(e){
 
     xhr.onload = function(){
         if(this.status === 200){
-            const response = this.responseText;
-            console.log(response);
+            const response = JSON.parse(this.responseText);
+            
+            let output = '';
+
+            if(response.type === 'success'){
+                response.value.forEach(function(joke){
+                    output += `<li>${joke.joke}</li>`;
+                });
+            }else {
+                output += '<li>Something went wrong </li>';
+            }
+
+            document.querySelector('.jokes').innerHTML = output;
         }
     }
 
